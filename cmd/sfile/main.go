76a0731f89e537370af55176c01168b68afc159a
吕海涛 <hi@taoshu.in>
2022-11-11 21:07:46 +0800

    add more flags

--- cmd/sfile/main.go
+++ cmd/sfile/main.go
@@ -10,18 +10,28 @@ import (
 
 func main() {
 	var root string
-	flag.StringVar(&root, "root", "", "file root path")
+	var addr string
+	var timeout time.Duration
+
+	flag.StringVar(&root, "root", "", "file root path, default is pwd")
+	flag.StringVar(&addr, "addr", "127.0.0.1:8080", "listen address and port")
+	flag.DurationVar(&timeout, "timeout", 10*time.Second, "connection keep alive timeout")
+
 	flag.Parse()
 
+	var err error
 	if root == "" {
-		root, _ = os.Getwd()
+		root, err = os.Getwd()
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	s := sfile.Server{}
 	s.Root = os.DirFS(root)
-	s.ReadTimeout = 10 * time.Second
+	s.ReadTimeout = timeout
 
-	if err := s.ListenAndServe("127.0.0.1:8080"); err != nil {
+	if err := s.ListenAndServe(addr); err != nil {
 		panic(err)
 	}
 }
